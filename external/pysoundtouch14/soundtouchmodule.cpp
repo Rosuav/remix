5ce467eecfb96f1f80aa2737ecc22d0d40d7d90c
Chris Angelico <rosuav@gmail.com>
2015-04-24 00:06:43 +1000

    And another - no format if not formatting

--- external/pysoundtouch14/soundtouchmodule.cpp
+++ external/pysoundtouch14/soundtouchmodule.cpp
@@ -187,7 +187,7 @@ SoundTouch_setChannels(SoundTouch *self, PyObject *args)
     }
   catch(std::runtime_error &error)
     {
-      PyErr_Format(PyExc_RuntimeError, error.what());
+      PyErr_SetString(PyExc_RuntimeError, error.what());
       return NULL;
     }
 
